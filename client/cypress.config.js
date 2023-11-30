e23c2a7f3a9dd62b4dab30afa84749fdc471d8cd
Anderson Ceci <andersonceci31@gmail.com>
2023-11-30 21:26:40 +0100

    change base url

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://event-organization-n2w5.onrender.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000"
   },
 });
